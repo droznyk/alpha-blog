c9833af8004c695f2de76d36768dd019a55946ab
droznyk <droznyk@gmail.com>
2017-11-13 17:44:08 +0000

    Refactor finding article redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :fetch_article, only: %w[ show edit update destroy ]
+  
   def index
     @articles = Article.all  
   end
@@ -17,16 +19,11 @@ class ArticlesController < ApplicationController
     end
   end
   
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
   
-  def edit
-    @article = Article.find(params[:id])  
-  end
+  def edit; end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -36,7 +33,6 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted!"
     redirect_to articles_path
@@ -47,4 +43,9 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :description)
   end
+  
+  def fetch_article
+    @article = Article.find(params[:id])
+  end
+  
 end
